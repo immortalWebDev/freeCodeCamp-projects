3b95e3063d32480db0934ed3433f5b66536f9eae
immortalWebDev <pgbadgujar007@gmail.com>
2025-01-12 18:00:04 +0530

    Added p2

---
+++ Legacy-JS-DSA/romanNumberConverter.js
@@ -0,0 +1,30 @@
+function convertToRoman(num) {
+  const map = [
+    { value: 1000, numeral: "M" },
+    { value: 900, numeral: "CM" },
+    { value: 500, numeral: "D" },
+    { value: 400, numeral: "CD" },
+    { value: 100, numeral: "C" },
+    { value: 90, numeral: "XC" },
+    { value: 50, numeral: "L" },
+    { value: 40, numeral: "XL" },
+    { value: 10, numeral: "X" },
+    { value: 9, numeral: "IX" },
+    { value: 5, numeral: "V" },
+    { value: 4, numeral: "IV" },
+    { value: 1, numeral: "I" },
+  ];
+
+  var roman = "";
+
+  for (var i = 0; i < map.length; i++) {
+    while (num >= map[i].value) {
+      roman = roman + map[i].numeral;
+      num = num - map[i].value;
+    }
+  }
+
+  return roman;
+}
+
+convertToRoman(36);
